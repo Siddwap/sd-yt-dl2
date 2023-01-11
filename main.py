58e93e7253f0996460e38a882f948135a5c09f06
Siddwap <58450503+Siddwap@users.noreply.github.com>
2023-01-11 15:22:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-from flask import Flask, render_template, request, send_file
+from fpdf import FPDF
+import json
 
-from pytube import YouTube
-
-import os
-
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-
-    return render_template('index.html')
-
-@app.route('/download', methods=['POST'])
-
-def download():
-
-    video_url = request.form['video_url']
-
-    yt = YouTube(video_url)
-
-    video = yt.streams.first()
-
-    print(video)
-
-    return send_file(f'{video.default_filename}', as_attachment=True, cache_timeout=0)
+@app.route('/convert-to-pdf', methods=['POST'])
+def convert_to_pdf():
+    form_data = json.loads(request.data)
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.text(10, 10, "Name:")
+    pdf.cell(40, 10, form_data["name"])
+    pdf.text(10, 20, "Email:")
+    pdf.cell(40, 20, form_data["email"])
+    pdf_content = pdf.output(dest='S').encode('latin1')
+    response = make_response(pdf_content)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename=form.pdf'
+    return response
 
 if __name__ == '__main__':
-
     app.run(debug=True)
 
